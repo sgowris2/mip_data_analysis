383d0311417f82ade9b59fd98223e759b42d57e5
sgowris2 <sgowris2@gmail.com>
2024-09-21 13:02:28 +0530

    Progress commit

--- code/main.py
+++ code/main.py
@@ -1,21 +1,29 @@
+import json
 import os
 import pandas as pd
 from pprint import pprint
 
 import utils
-from stats import run_stat_analysis
+import stats
 
 
-def run_analysis(filepath):
-    df = pd.read_csv(filepath, dtype=object)
+def run_analysis(directory, filename):
+    df = pd.read_csv(os.path.join(directory, filename), dtype=object)
     df = df.drop_duplicates()
     df = utils.normalize_column_names(df)
     df = utils.set_null_strings_to_none(df)
-    result = run_stat_analysis(df)
-    pprint(result)
+    dataset_stats = stats.dataset_stats(df)
+    task_evidence_stats = stats.task_evidence_stats(df)
+
+    pprint(dataset_stats)
+    pprint(task_evidence_stats)
+    with open('../output/dataset_stats__{}.json'.format(filename.replace('.csv', '')), 'w') as f:
+        json.dump(dataset_stats, f)
+    with open('../output/task_evidence_stats__{}.json'.format(filename.replace('.csv', '')), 'w') as f:
+        json.dump(task_evidence_stats, f)
 
 
 if __name__ == '__main__':
     directory = '../data'
     filename = 'mip_hr_2024-09.csv'
-    run_analysis(os.path.join(directory, filename))
+    run_analysis(directory, filename)
--- code/stats.py
+++ code/stats.py
@@ -1,22 +1,40 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def run_stat_analysis(df: pd.DataFrame):
-
+def dataset_stats(df: pd.DataFrame):
     empty_columns = df.columns[df.isnull().all()]
     column_stats = df.describe(include='all')
-    column_stats.rename(index={'count': 'total_no_of_values',
-                               'unique': 'no_of_unique_values',
-                               'top': 'most_common_value',
-                               'freq': 'no_of_instances_of_most_common_value'}, inplace=True)
+    result = {
+        'no_of_unique_rows': int(df.shape[0]),
+        'no_of_columns': int(df.shape[1]),
+        'column_names': df.columns.tolist(),
+        'empty_columns': 'None' if len(empty_columns) == 0 else empty_columns,
+        'column_stats': [{x: {'count': int(column_stats.loc['count'][x]),
+                              'unique': int(column_stats.loc['unique'][x]),
+                              'most_common': column_stats.loc['top'][x],
+                              'most_common_no_instances': int(column_stats.loc['freq'][x])}} for x in column_stats],
+    }
+    return result
+
+
+def task_evidence_stats(df: pd.DataFrame):
+
+    counts_teacher_project = df.groupby([df.uuid, df.project_title]).nunique()
+    counts_teacher_project['task_or_project_evidence'] = counts_teacher_project['task_evidence'] + counts_teacher_project[
+        'project_evidence']
+    histogram_submitted_tasks = counts_teacher_project['tasks'].value_counts().sort_index().to_dict()
+    histogram_submitted_evidences = counts_teacher_project['task_or_project_evidence'].value_counts().sort_index().to_dict()
+
+    counts_project_task = df.groupby([df.project_title, df.tasks]).nunique()
+    counts_project_task['task_or_project_evidence'] = counts_project_task['task_evidence'] \
+                                                         + counts_project_task['project_evidence']
+    counts_project_task['avg_evidences'] = counts_project_task['task_or_project_evidence'] / counts_project_task['uuid']
 
     result = {
-        'no_of_unique_rows': df.shape[0],
-        'no_of_columns': df.shape[1],
-        'column_names': df.columns.values,
-        'empty_columns': 'None' if len(empty_columns) == 0 else empty_columns,
-        'column_stats': [{x: dict(column_stats[x])} for x in column_stats],
+        'histogram_of_tasks_submitted_per_teacher-project': histogram_submitted_tasks,
+        'histogram_of_evidences_submitted_per_teacher-project': histogram_submitted_evidences,
+        'avg_no_of_evidences_submitted_per_task': {str(i): row['avg_evidences'] for i, row in counts_project_task.iterrows()}
     }
-    print(result)
     return result
